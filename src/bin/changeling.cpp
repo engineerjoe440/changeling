3e480c7767d299314abc585e68db12e03f92a9d0
Joe Stanley <33275230+engineerjoe440@users.noreply.github.com>
2024-07-28 10:10:55 -0700

    drop erroring code

--- src/bin/changeling.cpp
+++ src/bin/changeling.cpp
@@ -74,14 +74,6 @@ struct arguments {
 };
 
 static error_t parse_opt(int key, char *arg, struct argp_state *state) {
-    struct arguments *arguments = state->input;
-    switch (key) {
-    case 'l': arguments->mode = LINE_MODE; break;
-    case 'w': arguments->mode = WORD_MODE; break;
-    case 'i': arguments->isCaseInsensitive = true; break;
-    case ARGP_KEY_ARG: return 0;
-    default: return ARGP_ERR_UNKNOWN;
-    }   
     return 0;
 }
 
