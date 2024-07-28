e5cd6c2875b601b112f6b407d12804808016052b
Joe Stanley <33275230+engineerjoe440@users.noreply.github.com>
2024-07-28 11:03:31 -0700

    drop inline

--- src/bin/changeling.cpp
+++ src/bin/changeling.cpp
@@ -73,7 +73,7 @@ struct arguments {
   char *ipAddress;
 };
 
-inline bool ends_with(std::string const & value, std::string const & ending)
+bool ends_with(std::string const & value, std::string const & ending)
 {
     if (ending.size() > value.size()) return false;
     return std::equal(ending.rbegin(), ending.rend(), value.rbegin());
