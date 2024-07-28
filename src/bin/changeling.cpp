d187ec8a40a46dbfd8df77af3dcada41b967b150
Joe Stanley <33275230+engineerjoe440@users.noreply.github.com>
2024-07-28 11:09:25 -0700

    use test

--- src/bin/changeling.cpp
+++ src/bin/changeling.cpp
@@ -88,13 +88,13 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state) {
       break;
     
     case ARGP_KEY_ARG:
-      if (arguments->file != 0) {
-        fprintf(stderr, "File already provided: %s\n", arguments->file);
-        /* Too many arguments. */
-        argp_usage (state);
-      }
-
       if (ends_with(arg, ".wav")) {
+        if (arguments->file != 0) {
+          fprintf(stderr, "File already provided: %s\n", arguments->file);
+          /* Too many arguments. */
+          argp_usage (state);
+        }
+
         // String ends with `.wav` -- Must be the file
         fprintf(stderr, "File provided: %s\n", arg);
         arguments->file = arg;
